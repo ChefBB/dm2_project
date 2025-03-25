69d7ac27980dd8a7d05c0f900d34b651ef929865
Bazinga3 <b.barbieri7@studenti.unipi.it>
2025-03-25 15:35:36 +0100

    removed index from preprocessed dataset

---
+++ support.py
@@ -0,0 +1,18 @@
+import ast
+
+def convert_string_list(str_list: str) -> list[str]:
+    """
+    Converts a string in the format "['<str>' (, ...)]" to a list
+
+    Args:
+        str_list (str): string in the format "['<str>' (, ...)]"
+
+    Returns:
+        list[str]: the formatted data
+    """
+    stripped = str_list.strip()
+    if stripped == '[]':
+        return []
+    
+    country_list = ast.literal_eval(stripped)  # Convert string to actual list
+    return [f'{item}' for item in country_list]
