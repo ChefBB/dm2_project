f280a5866bcae0eee737826c63a6175057d006ce
Bazinga3 <b.barbieri7@studenti.unipi.it>
2025-03-30 17:07:53 +0200

    added way to frequency encoding separately on train/test sets+genre embedding exploration (still very much to work on)

--- support/countries_handling.py
+++ support/countries_handling.py
@@ -54,11 +54,13 @@ def get_encoded(country_list: list[str], name: str = '') -> dict[str, int]:
     return dict(continent_counts)
 
 
-def explode_continents_and_freq_enc(df: pd.DataFrame) -> pd.DataFrame:
+def explode_continents_and_freq_enc(df: pd.DataFrame, freq_enc: dict[str, float] | None=None) -> tuple[pd.DataFrame, dict[str, float]]:
     """
     Explodes the 'countryOfOrigin', 'regions' columns into separate rows for each continent.
     Counts the number of occurrences for each continent representative.
     Also frequency-encodes the 2 columns.
+    
+    For test set usage, use frequency encoding obtained on train set.
     
     Returns the modified dataframe.
     
@@ -68,19 +70,31 @@ def explode_continents_and_freq_enc(df: pd.DataFrame) -> pd.DataFrame:
     df : pd.DataFrame
         The dataframe
         
+    freq_enc : dict[str, float] | None
+        The frequency encoding for the columns
+        If None, it will be calculated.
+        If not None, it will be used to map the values.
+        
     Returns
     -------
     pd.DataFrame: The new columns
+    
+    dict[str, float]: The frequency encoding for the columns
+    ----------
     """
     result = pd.DataFrame()
+    if freq_enc is None:
+        freq_enc = {}
+        flag = True
     for feat in ['regions', 'countryOfOrigin']:
         # Explode the column to handle lists
         exploded = df[feat].explode()
         
         # Get frequency encoding for the column
-        freq_enc = exploded.value_counts(normalize=True).to_dict()
+        if flag:
+            freq_enc[feat] = exploded.value_counts(normalize=True).to_dict()
         result[f'{feat}_freq_enc'] = df[feat].map(
-            lambda x, freq_enc=freq_enc: sum(freq_enc.get(i, 0) for i in x))
+            lambda x, freq_enc=freq_enc[feat]: sum(freq_enc.get(i, 0) for i in x))
         
         # Apply the get_encoded function to the column
         encoded = df[feat].apply(get_encoded, name=feat)
@@ -89,4 +103,4 @@ def explode_continents_and_freq_enc(df: pd.DataFrame) -> pd.DataFrame:
         for key in encoded[0].keys():
             result[key] = [enc[key] for enc in encoded]
     
-    return result
+    return result, freq_enc
---
+++ support/embedding.py
@@ -0,0 +1,110 @@
+"""
+This module contains functions to generate embeddings for the genres of movies using Word2Vec.
+"""
+import pandas as pd
+from gensim.models import Word2Vec
+import numpy as np
+from node2vec import Node2Vec
+import networkx as nx
+
+
+
+# Step 2: Generate Embeddings for Each Sample
+def get_w2v_model(df: pd.DataFrame, name: str='models/genre_w2v_model.model') -> Word2Vec:
+    """
+    Generate the Word2Vec model for the genres column in the given DataFrame.
+    Save model to the specified name.
+    ----------
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame containing the genres.
+
+    name : str
+        The name of the model file.
+        
+    
+    ----------
+    Returns
+    -------
+    Word2Vec: The Word2Vec model.
+    """
+    w2v_model = Word2Vec(sentences=df['genre'], vector_size=100, window=3, min_count=1, workers=4, sg=0)
+    w2v_model.save(name)
+    
+    return w2v_model
+
+
+def get_genre_embedding(genre: list[str], w2v_model: Word2Vec) -> np.ndarray:
+    """
+    Generate the embedding for a single entry using the Word2Vec model.
+    ----------
+    Parameters
+    ----------
+    genre : list[str]
+        The genre to generate the embedding for.
+
+    w2v_model : Word2Vec
+        The Word2Vec model.
+        
+    
+    ----------
+    Returns
+    -------
+    np.ndarray: The embedding for the genre.
+    """
+    # Get the embeddings for each word in the genre
+    embeddings = [w2v_model.wv[word] for word in genre if word in w2v_model.wv]
+    
+    # Return the mean of the embeddings
+    return np.mean(embeddings, axis=0) if embeddings else np.zeros(w2v_model.vector_size)
+
+    def get_node2vec_model(graph: nx.Graph, name: str='models/genre_node2vec_model.model') -> Node2Vec:
+        """
+        Generate the Node2Vec model for the given graph.
+        Save model to the specified name.
+        ----------
+        Parameters
+        ----------
+        graph : nx.Graph
+            The graph representing the genres.
+
+        name : str
+            The name of the model file.
+            
+        ----------
+        Returns
+        -------
+        Node2Vec: The Node2Vec model.
+        """
+        node2vec = Node2Vec(graph, dimensions=100, walk_length=30, num_walks=200, workers=4)
+        model = node2vec.fit(window=10, min_count=1, batch_words=4)
+        model.save(name)
+        
+        return model
+
+
+    def get_node_embedding(node: str, node2vec_model: Node2Vec) -> np.ndarray:
+        """
+        Generate the embedding for a single node using the Node2Vec model.
+        ----------
+        Parameters
+        ----------
+        node : str
+            The node to generate the embedding for.
+
+        node2vec_model : Node2Vec
+            The Node2Vec model.
+            
+        ----------
+        Returns
+        -------
+        np.ndarray: The embedding for the node.
+        """
+        if node in node2vec_model.wv:
+            return node2vec_model.wv[node]
+        else:
+            return np.zeros(node2vec_model.wv.vector_size)
+
+# Apply the function to generate embeddings for each row
+# df['genre_embedding'] = df['genre'].apply(lambda x: get_genre_embedding(x, w2v_model))
